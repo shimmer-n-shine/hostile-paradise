893f48fe295142a71decff3c24c4dbd4bd4e73c8
shimmer-n-shine <artinnoyan@gmail.com>
2024-02-28 15:18:49 +0400

    disable sky island generation

--- World/DisableSkyIslands.cs
+++ World/DisableSkyIslands.cs
@@ -3,14 +3,14 @@ using Terraria.ModLoader;
 using Terraria.WorldBuilding;
 
 namespace hostileparadise.World;
-public class VanillaSkyGenerationSystem : ModSystem
+public class DisableSkyIslands : ModSystem
 {
     public override void ModifyWorldGenTasks(List<GenPass> tasks, ref double totalWeight)
     {
         int floatingIslandsIndex = tasks.FindIndex(genpass => genpass.Name.Equals("Floating Islands"));
-        int floatingIslandHousesIndex = tasks.FindIndex(genpass => genpass.Name.Equals("Floating Island Houses"));
+        if (floatingIslandsIndex != -1) tasks.RemoveAt(floatingIslandsIndex);
 
-        if (floatingIslandsIndex == -1) tasks.RemoveAt(floatingIslandsIndex);
-        if (floatingIslandHousesIndex == -1) tasks.RemoveAt(floatingIslandHousesIndex);
+        int floatingIslandHousesIndex = tasks.FindIndex(genpass => genpass.Name.Equals("Floating Island Houses"));
+        if (floatingIslandHousesIndex != -1) tasks.RemoveAt(floatingIslandHousesIndex);
     }
 }
