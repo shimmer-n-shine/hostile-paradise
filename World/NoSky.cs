0659e368b40681db37c2e4bf09f625543deef03e
artin <artin@THE-KAT-TREE>
2024-02-28 13:10:57 +0400

    Add project files.

---
+++ World/NoSky.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Terraria.ModLoader;
+using Terraria.WorldBuilding;
+
+namespace hostileparadise.World;
+public class VanillaSkyGenerationSystem : ModSystem
+{
+    public override void ModifyWorldGenTasks(List<GenPass> tasks, ref double totalWeight)
+    {
+        int floatingIslandsIndex = tasks.FindIndex(genpass => genpass.Name.Equals("Floating Islands"));
+        int floatingIslandHousesIndex = tasks.FindIndex(genpass => genpass.Name.Equals("Floating Island Houses"));
+
+        if (floatingIslandsIndex == -1) tasks.RemoveAt(floatingIslandsIndex);
+        if (floatingIslandHousesIndex == -1) tasks.RemoveAt(floatingIslandHousesIndex);
+    }
+}
---
+++ hostileparadise.cs
@@ -0,0 +1,12 @@
+using Terraria.ModLoader;
+using Terraria.WorldBuilding;
+
+namespace hostileparadise;
+
+public class hostileparadise : Mod
+{
+
+    public override void Load()
+    {
+    }
+}
